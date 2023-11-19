0df1b9622bddce7ee69304ce1e588bcfb5701da3
LolKek46578 <maks.borodynkin@list.ru>
2023-11-19 20:09:25 +0300

    cvxvbxfvb

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Helo, Word!");
+Console.WriteLine("Hllo, World!");
+Console.WriteLine("Hello, Wld!");
